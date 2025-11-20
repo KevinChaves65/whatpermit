c663fbb3ffa04e7a46d03fbe12d651f99526e566
KevinChaves65 <chaveskevin65@gmail.com>
2025-11-20 18:39:20 -0500

    added footer and headers

---
+++ frontend/src/lib/components/header.svelte
@@ -0,0 +1,14 @@
+<script>
+export let title = 'What Permit?';
+</script>
+
+
+<header class="flex justify-between items-center px-12 py-6 bg-white border-b">
+<div class="text-red-600 font-bold text-lg">{title}</div>
+<nav class="space-x-8 text-sm text-gray-900">
+<a href="/" class="hover:underline">Home</a>
+<a href="/lookup" class="hover:underline">Lookup</a>
+<a href="#" class="hover:underline">Who we are</a>
+<a href="#" class="hover:underline">My profile</a>
+</nav>
+</header>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/components/header.svelte';
+	import Footer from '$lib/components/footer.svelte';
 
 	let { children } = $props();
 </script>
@@ -9,4 +11,14 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div class="min-h-screen flex flex-col font-serif bg-white">
+<Header />
+
+
+<main class="flex-1">
+<slot />
+</main>
+
+
+<Footer />
+</div>
